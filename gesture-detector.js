f6c21b6a57c3c1976208f36c137fdea071d10026
ZhaiYM <57210938+ZhaiErGou@users.noreply.github.com>
2022-09-09 19:52:34 +0800

    Update gesture-detector.js

--- gesture-detector.js
+++ gesture-detector.js
@@ -164,10 +164,4 @@ AFRAME.registerComponent("gesture-detector", {
     return numberNames[Math.min(touchCount, 4) - 1];
   }
 });
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
+
